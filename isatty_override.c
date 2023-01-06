41cff5d82aef0d12be9b8dbe29ae5d5094ec5c99
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-01-06 01:05:09 +0100

    isatty_override.c reworked a bit more than the essential
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- isatty_override.c
+++ isatty_override.c
@@ -1,1 +1,24 @@
-int isatty(int x) { (void) x; return 1; }
+/**
+ *
+ * Copyright (c) Roberto A. Foglietta, 2023
+ *
+ * Authors:
+ *    Roberto A. Foglietta <roberto.foglietta@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+//int isatty(int x) { x = 0; return 1; }
+int isatty(int fd)  {
+    int flags = fcntl(fd, F_GETFD);
+    if (flags < 0) {
+        errno = EBADF;
+        return 0;
+    }
+    return 1;
+}
