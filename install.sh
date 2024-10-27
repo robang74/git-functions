c250ff1e01810eebb90d2185c05b36d307607d9d
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-10-27 05:41:21 +0100

    install.sh: SRCOLRS variable removed because unused
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- install.sh
+++ install.sh
@@ -28,7 +28,6 @@ cd ${TOPDIR}
 
 DESTDIR=".git-functions"
 SRCNAME=$(readlink -fem "$HOME/${DESTDIR}/git.shell")
-SRCOLRS=${SRCNAME/%git.shell/colors.shell}
 GITREPO="https://github.com/robang74/git-functions.git"
 SRCCMD="unset -f gitshell; test -r ${SRCNAME} && GFRELOAD=1 source ${SRCNAME} >/dev/null"
 BRANCH="$(git branch --show-current 2>&3 || echo main)"
